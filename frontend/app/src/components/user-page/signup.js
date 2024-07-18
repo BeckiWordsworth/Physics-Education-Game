f71e1f2beb2751080069c8df468d604cb8554d72
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-19 00:02:10 +0200

    Updating the signup page

--- frontend/app/src/components/user-page/signup.js
+++ frontend/app/src/components/user-page/signup.js
@@ -1,79 +1,67 @@
-import React from "react"
+import React, { useState } from "react";
 
-class SignUpForm extends React.Component {
+const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [newUserAdded, setNewUserAdded] = useState(false);
 
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    newUserAdded: false
-  }
-
-  handleFormUpdate = event => {
+  const handleFormUpdate = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  submitForm = event => {
-    event.preventDefault()
+  const submitForm = (event) => {
+    event.preventDefault();
 
     const userDetails = {
       username: this.state.username,
       email: this.state.email,
-      password: this.state.password
-    }
+      password: this.state.password,
+    };
 
     fetch("http://localhost:8080/users", {
       method: "POST",
       body: JSON.stringify(userDetails),
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(response => (
-        response.json()
-      ))
-      .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         if (result.created === true) {
-          console.log("Success:", JSON.stringify(result))
+          console.log("Success:", JSON.stringify(result));
           this.setState({
-            newUserAdded: true
-          })
+            newUserAdded: true,
+          });
         } else {
-          console.log("Failure:", JSON.stringify(result))
+          console.log("Failure:", JSON.stringify(result));
         }
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  render() {
-    return (
-      <div>
-        <h4>CREATE A NEW ACCOUNT</h4>
-        <form className="signupForm" onSubmit={this.submitForm}>
-          <label htmlFor="username">Username: </label>
-          <input name="username" type="text" onChange={this.handleFormUpdate} />
-          <br />
-          <label htmlFor="emal">Email address: </label>
-          <input name="email" type="text" onChange={this.handleFormUpdate} />
-          <br />
-          <label htmlFor="password">Password: </label>
-          <input name="password" type="password" onChange={this.handleFormUpdate} />
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-        {this.state.newUserAdded ?
-          <p>New user added. Welcome! Please check your email.</p>
-          :
-          <p />
-        }
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h4>CREATE A NEW ACCOUNT</h4>
+      <form className="signupForm" onSubmit={submitForm}>
+        <label htmlFor="username">Username: </label>
+        <input name="username" type="text" onChange={handleFormUpdate} />
+        <br />
+        <label htmlFor="emal">Email address: </label>
+        <input name="email" type="text" onChange={handleFormUpdate} />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input name="password" type="password" onChange={handleFormUpdate} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {this.state.newUserAdded ? <p>New user added. Welcome! Please check your email.</p> : <p />}
+    </div>
+  );
+};
 
-}
-
-export default SignUpForm
+export default SignUpForm;
