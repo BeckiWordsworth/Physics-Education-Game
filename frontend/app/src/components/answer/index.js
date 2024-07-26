33028eacdc3e9418b0d9fa47297b99aac4712ce8
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-26 23:19:37 +0200

    Answers page updating

--- frontend/app/src/components/answer/index.js
+++ frontend/app/src/components/answer/index.js
@@ -1,28 +1,53 @@
-import React from 'react'
-import './style.scss'
-
+import React, { useEffect, useState } from "react";
+// import "./style.scss";
+import styled from "styled-components";
 
 class Answer extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      answer: null
-    }
+      answer: null,
+    };
   }
 
   onClick = () => {
-    this.props.onAnswerSelected(this.props.index)
-  }
+    this.props.onAnswerSelected(this.props.index);
+  };
 
   render() {
     return (
-      <div className="answer">
-        <button id={this.props.index} onClick={this.onClick}>{this.props.answer}</button>
-      </div>
-    )
+      <D.Answer>
+        <button id={this.props.index} onClick={this.onClick}>
+          {this.props.answer}
+        </button>
+      </D.Answer>
+    );
   }
 }
 
-export default Answer
+export default Answer;
+
+const D = {
+  Answer: styled.div`
+    height: 100%;
+
+    button {
+      display: block;
+      background-color: #aaa;
+      font-size: 14pt;
+      color: #fff;
+      width: 100%;
+      height: 100%;
+      margin: 0px auto;
+      padding: 10px;
+      border-radius: 20px;
+      font-family: "Montserrat", sans-serif;
+      transition: 0.3s;
+    }
+
+    button:hover {
+      background-color: #20a8e9;
+    }
+  `,
+};
