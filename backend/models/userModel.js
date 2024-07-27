6c541c2a6a61f9e7f67168d85aba7ccb2777d3dd
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-27 23:56:30 +0200

    Begin adding models

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "User",
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: [true, "Please add a username"],
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please add the contact email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    accessToken: {
+      type: String,
+      default: () => uuid(),
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
