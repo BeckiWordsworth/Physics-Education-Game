ed7a2ce935789bdb1e057bf1da2dc191ae16aef8
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-27 23:56:56 +0200

    Little changes to the achievements

--- frontend/app/src/components/achievements/index.js
+++ frontend/app/src/components/achievements/index.js
@@ -1,8 +1,7 @@
-import React from "react"
-import './style.scss'
+import React from "react";
+import "./style.scss";
 
 class Achievements extends React.Component {
-
   render() {
     return (
       <div className="achievement-container">
@@ -10,12 +9,12 @@ class Achievements extends React.Component {
           <h1>{this.props.headline}</h1>
           <p>{this.props.text} </p>
         </div>
-        <div className={this.props.pointsData > this.props.pointsLimit ? "completed" : "badge"} >
+        <div className={this.props.pointsData > this.props.pointsLimit ? "completed" : "badge"}>
           <img src={"./" + this.props.source + ".png"} alt={this.props.source} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Achievements
+export default Achievements;
