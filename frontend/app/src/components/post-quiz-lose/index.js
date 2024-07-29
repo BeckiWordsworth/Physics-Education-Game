e64d6b5bb15896031b0ceeeb2f8fa31d4dea5807
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-29 22:51:20 +0200

    Loser page style update

--- frontend/app/src/components/post-quiz-lose/index.js
+++ frontend/app/src/components/post-quiz-lose/index.js
@@ -1,24 +1,49 @@
-import React from 'react'
-import './style.scss'
-
+import React from "react";
+import styled from "styled-components";
 
 class PostQuizLosePage extends React.Component {
-
-
   render() {
-
-    return(
-
-      <div className="loser-container">
+    return (
+      <L.LoserContainer>
         <h1> Game over! PLease try again!</h1>
 
         <img src={"./GameOver.png"} alt="loser" />
 
-        <a href={"/dashboard"}><button> Continue </button></a>
-      </div>
-    )
+        <a href={"/dashboard"}>
+          <button> Continue </button>
+        </a>
+      </L.LoserContainer>
+    );
   }
-
 }
 
-export default PostQuizLosePage
+export default PostQuizLosePage;
+
+const L = {
+  LoserContainer: styled.div`
+    padding: 50px;
+    text-align: center;
+    display: grid;
+
+    img {
+      padding: 10px;
+      margin: 0 auto;
+    }
+
+    button {
+      padding: 10px;
+      background: none;
+      border: solid 2px #bbb;
+      border-radius: 20px;
+      color: #666;
+      font-size: 10pt;
+      font-family: "Montserrat", sans-serif;
+      transition: 0.2s;
+    }
+
+    button:hover {
+      background: #ccc;
+      color: #333;
+    }
+  `,
+};
