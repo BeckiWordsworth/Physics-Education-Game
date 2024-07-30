38ed55adfd8e7a7e4e10119582614407680ed323
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-30 23:26:33 +0200

    Fixing the achievments container

--- frontend/app/src/components/achievements/index.js
+++ frontend/app/src/components/achievements/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./style.scss";
+// import "./style.scss";
+import styled from "styled-components";
 
 class Achievements extends React.Component {
   render() {
     return (
-      <div className="achievement-container">
+      <W.AchievmentContainer>
         <div className="champion-list">
           <h1>{this.props.headline}</h1>
           <p>{this.props.text} </p>
@@ -12,9 +13,40 @@ class Achievements extends React.Component {
         <div className={this.props.pointsData > this.props.pointsLimit ? "completed" : "badge"}>
           <img src={"./" + this.props.source + ".png"} alt={this.props.source} />
         </div>
-      </div>
+      </W.AchievmentContainer>
     );
   }
 }
 
 export default Achievements;
+
+const W = {
+  AchievmentContainer: styled.div`
+    margin: 0 auto;
+    width: 800px;
+    height: 170px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    padding: 10px 20px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 25px rgba(0, 0, 0, 0.04);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    img {
+      height: 150px;
+    }
+  `,
+
+  Badge: styled.div`
+    width: 20%;
+    filter: grayscale(100%);
+  `,
+
+  Completed: styled.div`
+    width: 20%;
+    filter: grayscale(0%);
+  `,
+};
