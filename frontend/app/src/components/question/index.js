be0e2c7c69fc98e7ba220f1e1921bee6da34dd61
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-31 23:40:52 +0200

    Updating question component

--- frontend/app/src/components/question/index.js
+++ frontend/app/src/components/question/index.js
@@ -1,21 +1,48 @@
-import React from 'react'
-import Header from '../header'
-import Answer from '../answer'
-import './style.scss'
-
+import React from "react";
+import Header from "../header";
+import Answer from "../answer";
+import styled from "styled-components";
 
 class Question extends React.Component {
-
   render() {
     return (
-      <div className="question-container">
-        <div className="question">
-          <div className="question-text">{this.props.question.text}</div>
-          <div className="question-id">{this.props.question._id}</div>
-        </div>
-      </div>
-    )
+      <Q.QuestionContainer>
+        <Q.Question>
+          <Q.QuestionText>{this.props.question.text}</Q.QuestionText>
+          <Q.QuestionId>{this.props.question._id}</Q.QuestionId>
+        </Q.Question>
+      </Q.QuestionContainer>
+    );
   }
 }
 
-export default Question
+export default Question;
+
+const Q = {
+  QuestionContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 440px;
+  `,
+
+  Question: styled.div`
+    width: 80%;
+    height: 300px;
+    background-color: #f3f3f3;
+    padding: 10px;
+    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.05);
+    border: solid 3px #e1e1e1;
+  `,
+
+  QuestionText: styled.div`
+    font-size: 19pt;
+    font-weight: bold;
+  `,
+  QuestionId: styled.div`
+    margin-top: 10px;
+    color: #ccc;
+    font-size: 10pt;
+  `,
+};
