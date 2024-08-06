2ebbe260152900d165d8ee13af8a50e46932c927
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-08-06 22:19:21 +0200

    Update progress bar

--- frontend/app/src/components/progressBar/index.js
+++ frontend/app/src/components/progressBar/index.js
@@ -1,28 +1,21 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import "./style.scss";
 
-
-class ProgressBar extends React.Component {
-
+const ProgressBar = () => {
   state = {
     active: false,
   };
 
-  render() {
+  let progressStyle = {
+    width: this.state.active ? "100%" : this.props.progress + "%",
+  };
 
-    let progressStyle = {
-      width: this.state.active ? "100%" : this.props.progress + "%"
-    }
+  return (
+    <div className="progressBar">
+      <div className="background"></div>
+      <div className="filler" style={progressStyle}></div>
+    </div>
+  );
+};
 
-    return(
-      <div className="progressBar">
-        <div className="background"></div>
-        <div className="filler" style={progressStyle}></div>
-      </div>
-
-    )
-  }
-
-}
-
-export default ProgressBar
+export default ProgressBar;
